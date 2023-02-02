c5fd58d6b90b9500664e3ed004670a92401a1757
Đỗ Cao Long <long@myspa.vn>
2023-02-02 09:00:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const port = 5050;
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    res.status(200).send({
+        message: "Hello world",
+    });
+});
+
+app.post("/", async (req, res) => {
+    const { prompt } = req.body;
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.7,
+            max_tokens: 4000,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+        // await PromiseTimeout(3000);
+        res.status(200).send({
+            bot: response.data.choices[0].text,
+        });
+    } catch (error) {
+        res.status(500).send({ error });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
