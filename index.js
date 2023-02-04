494ce6000650deae85bc366b0e5757920065d10c
Đỗ Cao Long <long@myspa.vn>
2023-02-04 08:46:39 +0700

    update config

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post("/", async (req, res) => {
 // app.listen(port, () => {
 //     console.log(`Listening on port ${port}`);
 // });
-var server = app.listen(app.get("port"), function () {
-    debug("Express server listening on port " + server.address().port);
+var server = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
-server.timeout = 10000;
+server.setTimeout(10000);
