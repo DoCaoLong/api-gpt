05011635505e149ee4efc7ba101911a5673540ca
Đỗ Cao Long <long@myspa.vn>
2023-02-04 08:34:25 +0700

    update config

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.post("/", async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 1,
-            max_tokens: 4000,
+            temperature: 0.5,
+            max_tokens: 2000,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
